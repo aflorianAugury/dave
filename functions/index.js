8f5a3ed28e800913471fc3789b44402e56b9cafa
Gal Ben-Haim <gbenhaim@augury.com>
2017-12-27 18:09:37 +0200

    added redirect url

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 
+exports.oauth2Redirect = functions.https.onRequest((request, response) => {
+});
+
 exports.dialogFlowWebhook = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase! (and sigal)");
 });
